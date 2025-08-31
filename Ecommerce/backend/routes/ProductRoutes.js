6a807be2395359d324a8ac3bec87956469e88e31
rohithchitturi2223 <nagatejarohith@gmail.com>
2025-08-31 12:38:52 +0530

    Delete Ecommerce directory

--- Ecommerce/backend/routes/ProductRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/productRoutes.js
-import express from 'express';
-import { getProducts } from '../controllers/productController.js';
-
-const router = express.Router();
-
-router.get('/products', getProducts);  // Route to fetch products by category and subcategory
-
-export default router;
